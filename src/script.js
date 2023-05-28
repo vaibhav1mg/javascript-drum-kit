77fec059fa08eb11238105f4a3da0adf304294bd
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-05-28 17:21:21 +0530

    Reformatting code

--- src/script.js
+++ src/script.js
@@ -1,10 +1,21 @@
-addEventListener('keydown', function(e) {
-    const audio = document.querySelector(`audio[id="${e.keyCode}"]`);
-    if(!audio) return;
+function playSound(e) {
+    const audio = document.querySelector(`audio[id="${e.keyCode || e.target.id}"]`);
+    if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-    const key = document.querySelector(`.btn[id="${e.keyCode}"]`);
-    key.classList.add('playing');
+    const key = document.querySelector(`.btn[id="${e.keyCode || e.target.id}"]`);
+    key.classList.add('ty');
 }
-);
 
+
+
+function removeTransition(e) {
+
+    this.classList.remove('ty');
+}
+
+const keys = document.querySelectorAll('.btn');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+addEventListener('keydown', playSound);
+addEventListener('click', playSound);
